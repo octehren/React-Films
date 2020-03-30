5d40e5fffa90f9b4ce4b75a40ea42eff04a6517b
Otavio <otamm@hotmail.com>
2020-03-30 02:19:59 -0300

    render method is the last in all class bodies

--- src/components/ButtonContent.js
+++ src/components/ButtonContent.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 
 class ButtonContent extends Component {
+    
+    handleClick = (e) => {
+        this.props.onClick(e);
+    }
+
     render() {
         return (
             <button type="button" btn={this.props.buttonID} onClick={this.handleClick} className={`button is-rounded ${this.props.isActive ? "is-focused" : ""}`} >{this.props.label}</button>
         )
-    }
-
-    handleClick = (e) => {
-        this.props.onClick(e);
     }
 }
 
