2d19e112c3ace29b1a90960941ef82877df675ac
Otavio <otamm@hotmail.com>
2020-03-29 23:02:07 -0300

    successfully fetching data from external API

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -4,12 +4,13 @@ import ButtonContent from './ButtonContent';
 let filmData = require("../filmData.json");
 filmData["tv_seasons"].forEach(function(series) 
     {
-        series["total_episodes"] = series.episodes.length;
+        series["totalEpisodes"] = series.episodes.length;
     }
 );
 const displayedContents = ["Movies", "TV Series"];
 const alternateColors = ["pink", "gray", "gold", "blue", "red"];
 const numColors = alternateColors.length;
+const apiKey = "5215a9b636f2a7eb6edef75d09a921da"; // ideally this would be hidden in a local variable
 
 class MainPage extends Component {
     constructor(props) {
@@ -17,13 +18,16 @@ class MainPage extends Component {
         this.state = {
             activeButton: 0,
             filmData: [filmData["films"], filmData["tv_seasons"]],
-            seriesPage: 1,
-            moviesPage: 1
+            respectiveContentPages: [1, 1], // index 0 is movies, index 1 is tv series, etc. correlates w/ displayedContents
+            externalFilmData: []
         }
     }
 
     componentDidMount() {
-
+        if (this.state.externalFilmData.length > 0) {
+            console.log(this.state.externalFilmData);
+            this.setState({externalFilmData: []});
+        }
     }
 
     toggleDisplayedContent = (clickedButton) => {
@@ -31,23 +35,67 @@ class MainPage extends Component {
         if (clickedButton !== activeButton) this.setState({ activeButton: (activeButton + 1) % 2});
     }
 
+    fetchNumOfTVShowEpisodes = (tvShow) => {
+        const url = `https://api.themoviedb.org/3/tv/${tvShow.id}?api_key=${apiKey}`;
+        let request = new Request(url);
+        request.headers.append('Content-Type', 'application/json');
+        return fetch(request)
+        .then((response) => {
+            return response.text();
+        }).then((text) => {
+            return JSON.parse(text)["number_of_episodes"];
+        });
+    }
+
+    fetchExternalContent = () => {
+        const contentIndex = this.state.activeButton; // 0 for movies, 1 for series
+        const pagetoFetch = this.state.respectiveContentPages[contentIndex]; 
+        const contentTypeParam = ["movie", "tv"][this.state.activeButton]; // 1 for movies, 2 for series
+        const url = `https://api.themoviedb.org/3/discover/${contentTypeParam}?api_key=${apiKey}&page=${pagetoFetch}`;
+        let request = new Request(url);
+        request.headers.append('Content-Type', 'application/json');
+        fetch(request)
+        .then((response) => {
+            return response.text();
+        }).then((text) => {
+            let results = JSON.parse(text)["results"].slice(0, 3);
+            if (contentTypeParam === "tv") {
+                let promises = [];
+                results.forEach((tvShow) => { 
+                    promises.push(this.fetchNumOfTVShowEpisodes(tvShow));
+                });
+                Promise.allSettled(promises).then((totalEpisodes) => {
+                    results.map((tvShow, index) => {
+                        tvShow.totalEpisodes = totalEpisodes[index];
+                    });
+                    this.setState({externalFilmData: results});
+                })
+            } else {
+                this.setState({externalFilmData: results});
+            }
+        })
+    }
+
     render() {
         return (
             <div>
                 <div className="container hero">
                     <div className="content-buttons">
-                    {   displayedContents.map((contentName, index) => {
-                            return (
-                                <ButtonContent 
-                                onClick={this.toggleDisplayedContent}
-                                label={contentName}
-                                buttonID={index}
-                                key={index} 
-                                isActive={this.state.activeButton === index} 
-                                />
-                            )
-                        })
-                    }
+                        <button class="button is-info is-round" onClick={this.fetchExternalContent}>
+                            See More {displayedContents[this.state.activeButton]}
+                        </button>
+                        {   displayedContents.map((contentName, index) => {
+                                return (
+                                    <ButtonContent 
+                                    onClick={this.toggleDisplayedContent}
+                                    label={contentName}
+                                    buttonID={index}
+                                    key={index} 
+                                    isActive={this.state.activeButton === index} 
+                                    />
+                                )
+                            })
+                        }
                     </div>
                 </div>
                 <div className="container">
@@ -60,7 +108,7 @@ class MainPage extends Component {
                                 description={film.description} 
                                 priceBuy={film.prices.buy} 
                                 priceRent={film.prices.rent} 
-                                totalEpisodes={film.total_episodes} 
+                                totalEpisodes={film.totalEpisodes} 
                                 />
                             );
                         })
