95ffa5f2f8987bb95e9e6d3ea4081493db3a0537
Otavio <otamm@hotmail.com>
2020-03-30 22:57:06 -0300

    debugged tests!

--- src/App.test.js
+++ src/App.test.js
@@ -3,14 +3,16 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe("initial render of objects", () => {
-  //const { getByText } = render(<App />);
+  const { getByText } = render(<App />);
   test("initial render of movies", () => {
-    const initialMovies = ["Mulholland Drive", "In the Mood for Love"]
+    let initialMovies = ["Mulholland Drive", "In the Mood for Love"]
     let renderedFilms = [];
     const htmlCollection = document.getElementsByClassName("title");
     for (let i = 0; i < htmlCollection.length; i++) {
       renderedFilms.push(htmlCollection[i].textContent);
     }
+    initialMovies.sort();
+    renderedFilms.sort();
     expect(initialMovies.every((value, index) => { return value === renderedFilms[index]; })).toEqual(true);
   });
 });
